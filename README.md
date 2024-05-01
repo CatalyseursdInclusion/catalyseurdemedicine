cda1de3e7bef7bbbc6878d9824b1ed878f7fbff5
a4r4v <aaravpal2028@sfhs.in>
2024-05-01 20:17:56 +0530

    Working version of both streams 1) tika and 2) heart disease predictons

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # catalyseurdemedicine
-A WIP program which scans a pdf prescription document and finds a generic version of the medicine prescribed
+
+First program - inside tika folder
+
+This program takes in a jpeg image of pharmacy invoice, and it extracts the medication names from it.
+
+To execute the code run following 
+
+python3 extract_medicine_name [-i input filename] [-h]
+or 
+./extract_medicine_name [-i input filename] [-h]
+
+where the input file name is in jpeg format.
+
+As an example - 
+python3 extract_medicine_name.py -i scan.jpeg
+or 
+./extract_medicine_name.py -i scan.jpeg
+
+Ensure the jpg file is in the same directory as the code.
+
+
+
+
+Second program - inside heartdisease folder
+
+This program trains a RandomForest model on heart disease dataset from UC Irvine repository
+Database url: https://archive.ics.uci.edu/dataset/45/heart+disease
+
+The dataset was downloaded and only "processed.cleveland.data" was used for model training
+There are 303 records with 14 columns including the target label
+the header record was missing, so we added a header record so that we can have the column names in pandas
+
+The "target" field refers to the presence of heart disease in the patient.  It is integer valued from 0
+(no presence) to 4. Experiments with the Cleveland database have concentrated on simply attempting 
+to distinguish presence (values 1,2,3,4) from absence (value 0) -- from heart-disease.names
+
+We created two version of the dataset uci_heart_mod.csv is with multiclass target and 
+uci_heart_mod_binary.csv is with binary target labels where we collapsed target label 1,2,3,4 into 1
---
+++ heartdisease/train_heart_model.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3 -W ignore
+#
+# (Aarav Pal, Adah Kapoor, Neel Bansal 2024)
+
+# imports
+import pandas as pd
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+
+#
+# Database url: https://archive.ics.uci.edu/dataset/45/heart+disease
+
+# The dataset was downloaded and only "processed.cleveland.data" was used for model training
+# There are 303 records with 14 columns including the target label
+# the header record was missing, so we added a header record so that we can have the column names in pandas
+
+# The "target" field refers to the presence of heart disease in the patient.  It is integer valued from 0
+# (no presence) to 4. Experiments with the Cleveland database have concentrated on simply attempting
+# to distinguish presence (values 1,2,3,4) from absence (value 0) -- from heart-disease.names
+
+# read in the data to pandas dataframe
+df = pd.read_csv('uci_heart_mod_binary.csv')
+
+# Create the Variables (X) and labels (y) dataframe
+y = df['target']
+X = df.drop('target', axis=1)
+
+
+# Create training (X, y) and test (X, y) datasets
+# Keeping test as 33% of all the data
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33)
+
+# Initialize RandomForest classifier and train it with x_train and y_train
+model = RandomForestClassifier(criterion='gini', max_depth=7, min_samples_leaf=5, n_estimators=200)
+model.fit(X_train, y_train)
+
+
+# Performance metrics - train & test
+trn_pred = model.predict(X_train)
+print("Training accuracy score of the model is:", accuracy_score(y_train, trn_pred) * 100, "%")
+
+tst_pred = model.predict(X_test)
+print("Testing accuracy score of the model is:", accuracy_score(y_test, tst_pred) * 100, "%")
